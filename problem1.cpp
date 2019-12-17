21bef3dfae72505e2fdbdfc1ab0fcf960a706460
yfksm <yfksm@users.noreply.github.com>
2019-12-17 17:57:32 +0900

    exclude 1000 from ans

--- problem1.cpp
+++ problem1.cpp
@@ -5,7 +5,7 @@ int main(void) {
     int N = 1000;
 
     int ans = 0;
-    for(int i = 1; i <= N; i++) {
+    for(int i = 1; i < N; i++) {
         if(i%3 == 0 or i%5 == 0){
             ans += i;
         }
