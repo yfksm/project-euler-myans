70948ec8307a0706e48971567dd862f6c4cafc99
yfksm <yfksm@users.noreply.github.com>
2019-12-17 17:47:52 +0900

    completed problem1

---
+++ problem1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int N = 1000;
+
+    int ans = 0;
+    for(int i = 1; i <= N; i++) {
+        if(i%3 == 0 or i%5 == 0){
+            ans += i;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
